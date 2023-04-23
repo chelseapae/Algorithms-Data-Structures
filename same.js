6eeca037ba56c3db6832de8249c94481cb4dfb4c
Chelsea Pae <chelseapae@gmail.com>
2023-04-23 16:51:13 -0700

    O(n) solution added

--- same.js
+++ same.js
@@ -1,24 +1,26 @@
 // Write a function called same, which accepts two arrays.
-// The function should return true if every value in the array has
-// it's corresponding value squared in the second array.
+// The function should return true if every value in the array has it's corresponding value squared in the second array.
 // The frequency of values must be the same.
 
-// Naive solution
+// same([1, 2, 3], [4, 1, 9]) - true
+
+/** O(n^2) solution **/
 function same(arr1, arr2) {
-  // if arr1 length doesn't eql arr2 length, return false
     if (arr1.length !== arr2.length) {
         return false;
     };
 
-  // iterate thru arr1
     for (let i = 0; i < arr1.length; i++) {
         let currentIndex = arr2.indexOf(arr1[i] ** 2)
-     // if the sqrd # isn't in arr2
         if (currentIndex === -1) {
             return false;
         }
-           // else, cut that number out of arr2
-            arr2.splice(currentIndex, 1)
+        arr2.splice(currentIndex, 1)
     }
     return true
+}
+
+/** O(n) solution **/
+function same2(arr1, arr2) {
+
 }
