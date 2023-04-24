3d61a6e05d9996d37b4f20f3baa6e7c70b8fe5a1
Chelsea Pae <chelseapae@gmail.com>
2023-04-23 19:02:43 -0700

    Complete anagrams

--- anagrams.js
+++ anagrams.js
@@ -5,3 +5,35 @@
 // validAnagram('', '') // true
 // validAnagram('aaz', 'zza') // false
 // validAnagram('qwerty', 'qwywrt') // true
+
+/** O(n) Solution **/
+function validAnagram(str1, str2){
+  let arr1 = str1.split('')
+  let arr2 = str2.split('')
+
+  if (arr1.length !== arr2.length) {
+      return false;
+  };
+
+  let counterObj1 = {};
+  let counterObj2 = {};
+
+  for (let i = 0; i < arr1.length; i++) {
+      counterObj1[arr1[i]] = (counterObj1[arr1[i]] || 0) + 1
+  };
+
+  for (let i = 0; i < arr2.length; i++) {
+      counterObj2[arr2[i]] = (counterObj2[arr2[i]] || 0) + 1
+  };
+
+  for (let key in counterObj1) {
+      if (!counterObj2[key]) {
+          return false;
+      };
+
+      if (counterObj1[key] !== counterObj2[key]) {
+          return false;
+      };
+  }
+  return true;
+}
