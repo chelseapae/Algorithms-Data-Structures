efc5396f7fc688a2c81fa2db09d06351aaed907c
Chelsea Pae <chelseapae@gmail.com>
2023-04-24 17:04:17 -0700

    O(n^2) solution

--- sumZero.js
+++ sumZero.js
@@ -7,3 +7,15 @@
 // sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3, 3]
 // sumZero([-2, 0, 1, 3]) // undefined
 // sumZero([1, 2, 3]) // undefined
+
+//** Time complexity - O(n^2) Solution && Space complexity is O(1) **//
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
