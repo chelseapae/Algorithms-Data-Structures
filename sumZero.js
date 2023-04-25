02f58ab1037e33d9dcde86c31a228153fb7a7152
Chelsea Pae <chelseapae@gmail.com>
2023-04-24 17:55:27 -0700

    Complete sumZero

--- sumZero.js
+++ sumZero.js
@@ -21,5 +21,17 @@ function sumZero(arr) {
 
 //** Time complexity - O(n) Solution && Space complexity is O(1) **//
 function sumZero2(arr) {
+  let left = 0;
+  let right = arr.length - 1;
 
+  while(left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left--;
+    }
+  }
 }
