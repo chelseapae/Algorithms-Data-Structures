b226693b63fa58c80f91563334fdf197fb27c90a
Chelsea Pae <chelseapae@gmail.com>
2023-04-24 18:52:10 -0700

    Add O(n) solution

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,6 +1,6 @@
 // Implement a function called countUniqueValues
 // which accepts a sorted array, and counts the unique values in the array.
-// There can be negative numbers in the array, but it wil always be sorted.
+// There can be negative numbers in the array, but it will always be sorted.
 
 // countUniqueValues([1, 1, 1, 1, 2]) // 2
 // countUniqueValues([]) // 0
@@ -8,5 +8,18 @@
 
 //** O(n) Solution **//
 function countUniqueValues(arr){
+  if (arr.length === 0) {
+      return 0;
+  }
 
+  let counter = {};
+  for (let i = 0; i < arr.length; i++) {
+      let current = arr[i];
+      if (!counter[current]) {
+          counter[current] = 1;
+      } else {
+          counter[current]++;
+      }
+  }
+  return Object.keys(counter).length;
 };
