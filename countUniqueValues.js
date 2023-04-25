a155acf72e45ff2c02d464e965df9b5f1104bf9a
Chelsea Pae <chelseapae@gmail.com>
2023-04-24 19:11:31 -0700

    Add countUniqueValues

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -23,3 +23,20 @@ function countUniqueValues(arr){
   }
   return Object.keys(counter).length;
 };
+
+//** O(n) Solution BUT using a different technique **//
+function countUniqueValues2(arr){
+  if (arr.length === 0) {
+      return 0;
+  };
+
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+      if (arr[i] !== arr[j]) {
+          i++;
+          arr[i] = arr[j];
+      };
+  };
+  //return arr.length - arr.indexOf(arr[i]);
+    return i + 1;
+};
