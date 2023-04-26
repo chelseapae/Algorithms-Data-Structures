59eaa7f63ea976cd1bb35fd4c25ea58090dccc98
Chelsea Pae <chelseapae@gmail.com>
2023-04-25 17:55:30 -0700

    Add maxSubArraySum

---
+++ maxSubArraySum.js
@@ -0,0 +1,12 @@
+// Write a function called maxSubArraySum which accepts
+// an array of integers and a number called n. The function should
+// calculate the maximum sum of n consecutive elements in the array.
+
+//maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2) //10
+//maxSubArraySum([4, 2, 1, 6], 1) //6
+//maxSubArraySum([4, 2, 1, 6, 2], 4) //13
+//maxSubArraySum([], 4) // null
+
+function maxSubArraySum(arr, num){
+
+}
