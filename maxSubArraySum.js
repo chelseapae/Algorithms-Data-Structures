6c9e13a86b9389a8180f5fb9d144ad96d6812409
Chelsea Pae <chelseapae@gmail.com>
2023-04-25 18:32:07 -0700

    Solve for O(n^2) Solution

--- maxSubArraySum.js
+++ maxSubArraySum.js
@@ -7,6 +7,21 @@
 //maxSubArraySum([4, 2, 1, 6, 2], 4) //13
 //maxSubArraySum([], 4) // null
 
+//** O(n^2) Solution **//
 function maxSubArraySum(arr, num){
+  if (num > arr.length) {
+    return null;
+  };
 
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    let temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i+j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
 }
