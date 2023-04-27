8e012418f2bbe6f71430ba90b53b21da0e390750
Chelsea Pae <chelseapae@gmail.com>
2023-04-26 18:36:21 -0700

    Complete maxSubArraySum

--- maxSubArraySum.js
+++ maxSubArraySum.js
@@ -33,16 +33,13 @@ function maxSubArraySum2(arr, num) {
   if (arr.length < num) {
     return null;
   };
-
   for (let i = 0; i < num; i++) {
     maxSum += arr[i];
   };
   let tempSum = maxSum;
-
   for (let i = num; i < arr.length; i++) {
-    tempSum = tempSum - arr[i - num] + arr[i];
+    tempSum = maxSum - arr[i - num] + arr[i];
     maxSum = Math.max(maxSum, tempSum);
   }
-
   return maxSum;
 }
