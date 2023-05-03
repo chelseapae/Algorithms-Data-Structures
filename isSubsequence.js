9c217c6ea0f9400dd3db41dee5daac63e08d027e
Chelsea Pae <chelseapae@gmail.com>
2023-05-02 18:09:37 -0700

    Complete isSubsequence

--- isSubsequence.js
+++ isSubsequence.js
@@ -6,3 +6,24 @@
 //isSubsequence('sing', 'sting'); // true
 //isSubsequence('abc', 'abracadabra'); // true
 //isSubsequence('abc', 'acb'); // false (order matters)
+
+//** O(n) Solution **//
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+
+  if (!str1){
+   return true;
+  };
+
+  while (j < str2.length) {
+   if (str1[i] === str2[j]){
+    i++;
+   }
+   if (i === str1.length) {
+    return true;
+   }
+   j++;
+  }
+  return false;
+ }
