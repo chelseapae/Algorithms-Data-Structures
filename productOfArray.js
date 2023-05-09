96b9da51810aab27a5c21ad84f12fc760a768f87
Chelsea Pae <chelseapae@gmail.com>
2023-05-08 19:08:07 -0700

    Add productOfArray

---
+++ productOfArray.js
@@ -0,0 +1,7 @@
+//Write a function called productOfArray which takes in an array of numbers
+//and returns the product of them all.
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+//** O(n) Solution **//
