aeffd6d02a7a004b80e761884178bdcf82d6572d
Chelsea Pae <chelseapae@gmail.com>
2023-05-08 18:56:47 -0700

    Complete power

--- power.js
+++ power.js
@@ -1,8 +1,21 @@
 //Write a function called power which accepts a base and an exponent.
 //The function should return the power of the base to the exponent.
-//This function should mimic the functionality of Math.pow().
-//do not worry about negative bases and exponents.
+//This function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents.
 
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+//** O(n) Solution **//
 function power(base, exp){
+  if (exp === 0) {
+      return 1
+  }
 
+ let result = 1;
+ while (exp > 0) {
+     result *= base
+     exp--
+ }
+ return result
 }
