d27c11e94d9b6b0cc5f86e9a22dc35c412296130
Chelsea Pae <chelseapae@gmail.com>
2023-05-10 17:22:58 -0700

    Complete factorial

--- factorial.js
+++ factorial.js
@@ -21,3 +21,12 @@ function factorial(num){
    }
    return result;
 }
+
+//** Recursive Solution **//
+function factorial(num){
+    if (num === 0) {
+        return 1;
+    };
+
+    return num * factorial(num - 1)
+ }
