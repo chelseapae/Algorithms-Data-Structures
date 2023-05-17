e51388a86efa7f4237dfdf0972ca861228821bae
Chelsea Pae <chelseapae@gmail.com>
2023-05-16 18:47:29 -0700

    Add parity

---
+++ parity.js
@@ -0,0 +1,9 @@
+//Given an array, check that every number switches from even to odd.
+//If there is a number that breaks the pattern, return the index of that number.
+
+//[100, 100, 100, 100] // 1
+//[1, 2, 5, 3, 4] // 3
+
+function parity(array) {
+
+}
