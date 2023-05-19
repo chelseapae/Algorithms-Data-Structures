36854e2b455ed29437688898ba6bc5c588e4c0f8
Chelsea Pae <chelseapae@gmail.com>
2023-05-18 18:27:02 -0700

    Complete binarySearch

--- binarysearch.js
+++ binarysearch.js
@@ -17,6 +17,21 @@
 //   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
 // ], 100) // -1
 
-function binarySearch(arr, val) {
+function binarySearch(arr, elem) {
+  let start = 0;
+  let end = arr.length -1;
+  let middle = Math.floor((start + end) / 2);
 
+  while(arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem){
+    return middle;
+  };
+  return -1
 }
