f077605c1e1ac4a9820d7581bf94cd9318138033
Chelsea Pae <chelseapae@gmail.com>
2023-05-21 20:35:13 -0700

    Complete naiveStringSearch

--- naiveStringSearch.js
+++ naiveStringSearch.js
@@ -1,6 +1,20 @@
 //Count the number of times a substring exists in a string.
 //If it doesn't exist, return -1
 
-function naiveStringSearch(str1, str2) {
+//naiveStringSearch("lorie loled", "lol") //1
 
+function naiveStringSearch(long, short) {
+  let count = 0;
+
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++){
+      if (short[j] !== long[i+j]) {
+        break;
+      }
+      if (j === short.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
