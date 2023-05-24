d481875a289bd24e3ec692c0a102352ca1c8905f
Chelsea Pae <chelseapae@gmail.com>
2023-05-23 18:59:29 -0700

    Complete bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,7 +1,18 @@
 //Given an array, sort it from smallest to largest, using a 'Bubblesort' technique
 
 //bubbleSort([5, 2, 1, 4, 6]) // [1, 2, 4, 5, 6]
+//bubbleSort([37, 45, 29, 8])
 
-function bubbleSort() {
-
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
 }
+
