a835cca3cf7503dbbd59c79782d2445d1bbf151d
Chelsea Pae <chelseapae@gmail.com>
2023-05-24 18:25:22 -0700

    Complete selectionSort

--- selectionSort.js
+++ selectionSort.js
@@ -2,6 +2,17 @@
 
 //selectionSort([5, 3, 4, 1, 2]) //[1, 2, 3, 4, 5]
 
-function selectionSort(){
-
+function selectionSort(arr){
+  for (var i = 0; i < arr.length; i++){
+    let lowest = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    var temp = arr[i];
+    arr[i] = arr[lowest];
+    arr[lowest] = temp;
+  }
+  return arr;
 }
