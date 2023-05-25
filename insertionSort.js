d876d5f080f509b53623ee52875f617a03ea386e
Chelsea Pae <chelseapae@gmail.com>
2023-05-24 20:00:43 -0700

    Begin solving

--- insertionSort.js
+++ insertionSort.js
@@ -7,6 +7,14 @@
   //[1, 3, 4, 5, 2]
   //[1, 2, 3, 4, 5]
 
-function insertionSort(arr){
+//insertionSort([])
 
+function insertionSort(arr){
+  for (var i = 1; i < arr.length; i++){
+    var currentVal = arr[i];
+    for (var j = i - 1; j >= 0; j--){
+
+    }
+  }
+  return arr;
 }
