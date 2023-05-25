2d1173add2f0c75cf24573149498e345ae479851
Chelsea Pae <chelseapae@gmail.com>
2023-05-24 18:02:22 -0700

    Add selectionSort

---
+++ selectionSort.js
@@ -0,0 +1,7 @@
+//Given an array, sort it by placing smallest values into sorted position first AKA using Selection Sort
+
+//selectionSort([5, 3, 4, 1, 2]) //[1, 2, 3, 4, 5]
+
+function selectionSort(){
+
+}
