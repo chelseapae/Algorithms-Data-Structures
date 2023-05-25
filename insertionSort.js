de972b56d53ca9c83bcf6987fda6bc4ef5f35a24
Chelsea Pae <chelseapae@gmail.com>
2023-05-24 19:41:46 -0700

    Add insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,8 @@
+//Given an array, sort it by building up the sort by gradually creating a larger left half which is always sorted
+
+//insertionSort([5, 3, 4, 1, 2])
+//ex of how it's sorted:
+  //[3, 5, 4, 1, 2]
+  //[3, 4, 5, 1, 2]
+  //[1, 3, 4, 5, 2]
+  //[1, 2, 3, 4, 5]
