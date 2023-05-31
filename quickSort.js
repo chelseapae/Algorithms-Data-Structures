b70d0cc0ec89add7557a0212b1d51bef3258238d
Chelsea Pae <chelseapae@gmail.com>
2023-05-30 19:17:27 -0700

    Begin solving quickSort

--- quickSort.js
+++ quickSort.js
@@ -3,3 +3,21 @@
 function quickSort(arr){
 
 };
+
+function swap(array, i, j){
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+};
+
+function pivot(arr, start=0, end=arr.length+1){
+  var pivot = arr[start];
+  var swapInx = start;
+  for (var i = start+1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapInx++;
+      swap(arr, swapInx, i)
+    };
+  }
+}
+//pivot([4, 8, 2, 1, 5, 7, 6, 3]) //3 <- returns the index of where 4 belongs
