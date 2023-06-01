bf83841bd2209aa400b5288db1443f977be01c01
Chelsea Pae <chelseapae@gmail.com>
2023-05-31 18:15:15 -0700

    Complete quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,7 +1,12 @@
 //Given an array, sort it in ascending order, using quick sort.
 
-function quickSort(arr){
-
+function quickSort(arr, left=0, right=arr.length-1){
+  if (left < right){
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex-1);
+    quickSort(arr, pivotIndex+1, right);
+  }
+  return arr;
 };
 
 function pivot(arr, start=0, end=arr.length+1){
