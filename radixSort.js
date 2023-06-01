1b1f7140aa17b88134c28c87f21a295026e5be8e
Chelsea Pae <chelseapae@gmail.com>
2023-05-31 19:31:49 -0700

    Complete getDigit helper

--- radixSort.js
+++ radixSort.js
@@ -3,3 +3,14 @@
 function radixSort(){
 
 };
+
+//getDigit(num, place) // returns the digit in num at the given place value
+//getDigit(12345, 0) //5
+//getDigit(12345, 1) //4
+//getDigit(12345, 2) //3
+//getDigit(12345, 5) //0
+
+function getDigit(num, place){
+  return Math.floor(Math.abs(num)/Math.pow(10, i)) % 10;
+}
+
