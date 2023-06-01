a2861e6b9c01d47f81c96d32bc036911826f104a
Chelsea Pae <chelseapae@gmail.com>
2023-05-31 19:39:38 -0700

    Complete mostDigits helper

--- radixSort.js
+++ radixSort.js
@@ -34,3 +34,11 @@ function digitCount(num){
 //mostDigits([1234, 56, 7]) //4
 //mostDigits([1, 1, 11111, 1]) //5
 //mostDigits([12, 34 56, 78]) //2
+
+function mostDigits(nums){
+  let maxDigits = 0;
+  for (var i = 0; i < nums.length; i++){
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
