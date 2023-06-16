3392762ce14beacf3c6c926f9179dcccb0914643
Chelsea Pae <chelseapae@gmail.com>
2023-06-15 19:37:34 -0700

    Add SinglyLinkedList

---
+++ singlyLinkedList.js
@@ -0,0 +1,18 @@
+//implement a singly linked list
+
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+}
+
