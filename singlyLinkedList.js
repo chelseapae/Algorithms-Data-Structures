f3277a1f78ec8baa3835f0e5ee3df745f27e775b
Chelsea Pae <chelseapae@gmail.com>
2023-06-24 18:40:42 -0700

    Add get

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -76,10 +76,14 @@ class SinglyLinkedList{
 
   //takes in an index and returns the val in that index
   get(index){
-    if (index < 0 || index >= this.length){
-      return null;
+    if (index < 0 || index >= this.length) return null;
+    var counter = 0;
+    var current = this.head;
+    while(counter !== index) {
+      current = current.next;
+      counter++;
     }
-
+    return current;
   }
 }
 
