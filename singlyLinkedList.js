48fd4275a6d1cfb4b8daca53329ff14de64beaf1
Chelsea Pae <chelseapae@gmail.com>
2023-06-25 17:29:17 -0700

    Complete insert

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -97,8 +97,17 @@ class SinglyLinkedList{
   }
 
   //Add a node to a specified index
-  insert(){
+  insert(index, val){
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return this.push(val)
+    if (index === 0) return this.unshift(val)
 
+    let newNode = new Node(val);
+    let prev = this.get(index) - 1;
+    let temp = prev.next;
+    newNode.next = temp;
+    this.length++;
+    return true;
   }
 }
 
