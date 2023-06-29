a772d9d324c628e62624533c4fd72cefacc95541
Chelsea Pae <chelseapae@gmail.com>
2023-06-28 17:17:53 -0700

    Complete unshift

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -58,8 +58,18 @@ class DoublyLinkedList{
     return prevHead;
   }
 
-  //
-  unshift(){
-
+  //Add a node to the beginning of the list
+  unshift(val){
+    let newNode = new Node(val);
+    if (this.length === 0){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
