900a5405798425c3599ab0915c6cefa433253aec
Chelsea Pae <chelseapae@gmail.com>
2023-07-19 19:13:54 -0700

    Add push

--- stack.js
+++ stack.js
@@ -2,17 +2,32 @@
 //can use push+pop or unshift+shift in arrays to create a "stack"
 //or, can create one, for example using a linked list
 
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Stack {
   constructor(){
     this.first = null;
     this.last = null;
     this.size = 0
   }
-}
 
-class Node {
-  constructor(value){
-    this.value = value;
-    this.next = null;
+  push(val){
+    let newNode = new Node(val);
+    if (this.size === 0){
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    this.size++;
+    return this.size;
+    //return ++this.size;
   }
 }
