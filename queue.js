c812166b2ab46606a5e11ba21db4607cf522fbf5
Chelsea Pae <chelseapae@gmail.com>
2023-07-20 19:07:40 -0700

    Add base

---
+++ queue.js
@@ -0,0 +1,16 @@
+//FIFO - First in, first out (like waiting in line)
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
