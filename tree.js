0620813f21338bea6ceccd0cada017eb86b360be
Chelsea Pae <chelseapae@gmail.com>
2023-07-31 19:04:52 -0700

    Complete DFSPreOrder

--- tree.js
+++ tree.js
@@ -71,8 +71,19 @@ class BinarySearchTree {
     return data;
   }
 
-  depthFirstSearch(){
+  //depth first search pre-order
+  DFSPreOrder(){
+    var data = [];
+    var current = this.root;
 
+    function traverse(node){
+      data.push(node);
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+    }
+
+    traverse(current);
+    return data;
   }
 
 };
