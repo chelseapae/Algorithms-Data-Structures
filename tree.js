3c5fc3bbc13b346bb38e069074991406754fdffc
Chelsea Pae <chelseapae@gmail.com>
2023-07-31 18:29:48 -0700

    Complete BreadthFirstSearch

--- tree.js
+++ tree.js
@@ -57,7 +57,18 @@ class BinarySearchTree {
   }
 
   BreadthFirstSearch(){
-
+    var data = [];
+    var queue = [];
+    var node = this.root;
+    queue.push(node);
+    while(queue.length){
+      node = queue.shift();
+      //data.push(node.value) <- same thing, but easier to see
+      data.push(node);
+      if(node.left) queue.push(node.left);
+      if(node.right) queue.push(node.right);
+    }
+    return data;
   }
 };
 
