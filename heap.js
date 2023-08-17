ec0ea291473cc5c80d42c75442816f93ebfecc0d
Chelsea Pae <chelseapae@gmail.com>
2023-08-16 20:07:52 -0700

    Add heap

---
+++ heap.js
@@ -0,0 +1,16 @@
+//maxBinaryHeap
+
+class maxBinaryHeap {
+  constructor(){
+    this.values = [];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+
+  }
+}
+
+//[41]
